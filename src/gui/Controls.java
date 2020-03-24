6035a6a635a592c142403a5b345ae88673f32a27
Thibault Serti <thibault.serti@gmail.com>
2020-03-24 10:29:44 +0100

    Removed unused imports

--- src/gui/Controls.java
+++ src/gui/Controls.java
@@ -13,7 +13,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.StringJoiner;
 
 import static gui.Settings.loadFile;
