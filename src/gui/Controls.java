83c233e491596f916056da15508e2f24322158c4
Thibault Serti <thibault.serti@gmail.com>
2020-03-24 10:28:53 +0100

    Add sliders to control parameters of the algorithms

--- src/gui/Controls.java
+++ src/gui/Controls.java
@@ -13,17 +13,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.StringJoiner;
 
 import static gui.Settings.loadFile;
 
 
 public class Controls extends JPanel {
-    private JRadioButton algo1, algo2, algo3;
+    private JRadioButton algo1RadioButton, algo2RadioButton, algo3RadioButton;
     private RoadMap roadMap;
     private ArrayList<City> cities;
     private int nbCities = 7;
+
+    // Taboo paramètres d'initialisation
+    int numberOfIterationsTaboo = 500;
+
+    // Recuit-Simulé paramètres d'initialisation
+    double startingTemperature = 10;
+    int numberOfIterationsRS = 1000;
+    double coolingRate = 0.99;
+
+    // Génétique paramètres d'initialisation
+    double mutationRate = 0.015;
+    int arenaSize = 5;
+    int numberOfIterationsAG = 100;
+    int populationSize = 50;
 
     public Controls(RoadMap roadMap) {
         super();
@@ -48,15 +62,10 @@ public class Controls extends JPanel {
         slideNbCities.setPaintLabels(true);
         slideNbCities.setMinorTickSpacing(1);
         slideNbCities.setMajorTickSpacing(5);
-        slideNbCities.addChangeListener(new ChangeListener(){
-            public void stateChanged(ChangeEvent event){
-                labelNbCities.setText("Nombre de villes : " + ((JSlider)event.getSource()).getValue());
-                nbCities = ((JSlider)event.getSource()).getValue();
-            }
-        });
         panNbCities.add(labelNbCities);
         panNbCities.add(slideNbCities);
-        this.add(panNbCities,BorderLayout.NORTH);
+        this.add(panNbCities, BorderLayout.NORTH);
+
         // Choix des données
         JPanel panData = new JPanel();
         panData.setBorder(BorderFactory.createTitledBorder("Choix des données"));
@@ -68,30 +77,169 @@ public class Controls extends JPanel {
         panData.add(scriptButton);
         this.add(panData, BorderLayout.NORTH);
 
-        // Boutons radios
+        // Choix de l'algorithme
         JPanel panAlgos = new JPanel();
         panAlgos.setBorder(BorderFactory.createTitledBorder("Choix des algorithmes"));
         panAlgos.setPreferredSize(new Dimension(350, 150));
         panAlgos.setLayout(new GridLayout(3, 1));
-        algo1 = new JRadioButton("Algorithme Tabou");
-        algo1.setSelected(true);
-        algo2 = new JRadioButton("Algorithme Recuit-Simulé");
-        algo3 = new JRadioButton("Algorithme Génétique");
+        algo1RadioButton = new JRadioButton("Algorithme Tabou");
+        //algo1RadioButton.setSelected(true);
+        algo2RadioButton = new JRadioButton("Algorithme Recuit-Simulé");
+        algo3RadioButton = new JRadioButton("Algorithme Génétique");
 
         ButtonGroup bg = new ButtonGroup();
-        bg.add(algo1);
-        bg.add(algo2);
-        bg.add(algo3);
-        panAlgos.add(algo1);
-        panAlgos.add(algo2);
-        panAlgos.add(algo3);
+        bg.add(algo1RadioButton);
+        bg.add(algo2RadioButton);
+        bg.add(algo3RadioButton);
+        panAlgos.add(algo1RadioButton);
+        panAlgos.add(algo2RadioButton);
+        panAlgos.add(algo3RadioButton);
         this.add(panAlgos, BorderLayout.NORTH);
-        // Boutons run
+
+        // -----Configurations des algorithmes-----
+        JPanel panConfig = new JPanel();
+        panConfig.setBorder(BorderFactory.createTitledBorder("Choix des algorithmes"));
+        panConfig.setPreferredSize(new Dimension(350, 240));
+        this.add(panConfig, BorderLayout.NORTH);
+
+        // Taboo
+        // Slide Nb iterations
+        JLabel labelNbIterationsTaboo = new JLabel("Nombre d'itérations : " + numberOfIterationsTaboo);
+        JSlider slideNbIterationsTaboo = new JSlider();
+        slideNbIterationsTaboo.setMaximum(1000);
+        slideNbIterationsTaboo.setMinimum(100);
+        slideNbIterationsTaboo.setValue(numberOfIterationsTaboo);
+        slideNbIterationsTaboo.setPaintTicks(true);
+        slideNbIterationsTaboo.setMinorTickSpacing(50);
+        slideNbIterationsTaboo.setMajorTickSpacing(100);
+        slideNbIterationsTaboo.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                labelNbIterationsTaboo.setText("Nombre d'itérations : " + ((JSlider) event.getSource()).getValue());
+                numberOfIterationsTaboo = ((JSlider) event.getSource()).getValue();
+            }
+        });
+
+        // Recuit-Simulé
+        // Slide Nb iterations
+        JLabel labelNbIterationsRS = new JLabel("Nombre d'itérations : " + numberOfIterationsRS);
+        JSlider slideNbIterationsRS = new JSlider();
+        slideNbIterationsRS.setMaximum(10000);
+        slideNbIterationsRS.setMinimum(1000);
+        slideNbIterationsRS.setValue(numberOfIterationsRS);
+        slideNbIterationsRS.setPaintTicks(true);
+        slideNbIterationsRS.setMinorTickSpacing(500);
+        slideNbIterationsRS.setMajorTickSpacing(1000);
+        slideNbIterationsRS.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                labelNbIterationsRS.setText("Nombre d'itérations : " + ((JSlider) event.getSource()).getValue());
+                numberOfIterationsRS = ((JSlider) event.getSource()).getValue();
+            }
+        });
+
+        // Slide Température initiale
+        JLabel labelStartingTemperature = new JLabel("Température initiale : " + (int) startingTemperature);
+        JSlider slideStartingTemperature = new JSlider();
+        slideStartingTemperature.setMaximum(50);
+        slideStartingTemperature.setMinimum(0);
+        slideStartingTemperature.setValue((int)startingTemperature);
+        slideStartingTemperature.setPaintTicks(true);
+        slideStartingTemperature.setPaintLabels(true);
+        slideStartingTemperature.setMinorTickSpacing(5);
+        slideStartingTemperature.setMajorTickSpacing(10);
+        slideStartingTemperature.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                labelStartingTemperature.setText("Température initiale : " + ((JSlider) event.getSource()).getValue());
+                startingTemperature = ((JSlider) event.getSource()).getValue();
+            }
+        });
+
+        // Slide Taux de refroidissement
+        JLabel labelCoolingRate = new JLabel("Taux de refroidissement : " + coolingRate);
+        JSlider slideCoolingRate = new JSlider();
+        slideCoolingRate.setMaximum(1000);
+        slideCoolingRate.setMinimum(950);
+        slideCoolingRate.setValue(((int)(coolingRate*1000)));
+        slideCoolingRate.setPaintTicks(true);
+        slideStartingTemperature.setPaintLabels(true);
+        slideCoolingRate.setMinorTickSpacing(5);
+        slideCoolingRate.setMajorTickSpacing(10);
+        slideCoolingRate.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                labelCoolingRate.setText("Taux de refroidissement : " + (double)((JSlider) event.getSource()).getValue() / 1000.0);
+                coolingRate = (double)((JSlider) event.getSource()).getValue() / 1000.0;
+            }
+        });
+
+        // Génétique
+        // Slide Nb iterations
+        JLabel labelNbIterationsAG = new JLabel("Nombre d'itérations : " + numberOfIterationsAG);
+        JSlider slideNbIterationsAG = new JSlider();
+        slideNbIterationsAG.setMaximum(1000);
+        slideNbIterationsAG.setMinimum(100);
+        slideNbIterationsAG.setValue(numberOfIterationsAG);
+        slideNbIterationsAG.setPaintTicks(true);
+        slideNbIterationsAG.setMinorTickSpacing(50);
+        slideNbIterationsAG.setMajorTickSpacing(100);
+        slideNbIterationsAG.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                labelNbIterationsAG.setText("Nombre d'itérations : " + ((JSlider) event.getSource()).getValue());
+                numberOfIterationsAG = ((JSlider) event.getSource()).getValue();
+            }
+        });
+
+        // Slide Taille de la population
+        JLabel labelPop = new JLabel("Taille de la population : " + populationSize);
+        JSlider slidePop = new JSlider();
+        slidePop.setMaximum(200);
+        slidePop.setMinimum(50);
+        slidePop.setValue(populationSize);
+        slidePop.setPaintTicks(true);
+        slidePop.setMinorTickSpacing(10);
+        slidePop.setMajorTickSpacing(50);
+        slidePop.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                labelPop.setText("Taille de la population : " + ((JSlider) event.getSource()).getValue());
+                populationSize = ((JSlider) event.getSource()).getValue();
+            }
+        });
+
+        // Slide Taille de l'arène
+        JLabel labelArenaSize = new JLabel("Taille de l'arène : " + arenaSize);
+        JSlider slideArenaSize = new JSlider();
+        slideArenaSize.setMaximum(10);
+        slideArenaSize.setMinimum(2);
+        slideArenaSize.setValue(arenaSize);
+        slideArenaSize.setPaintTicks(true);
+        slideArenaSize.setMinorTickSpacing(1);
+        slideArenaSize.setMajorTickSpacing(2);
+        slideArenaSize.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                labelArenaSize.setText("Taille de l'arène : " + ((JSlider) event.getSource()).getValue());
+                arenaSize = ((JSlider) event.getSource()).getValue();
+            }
+        });
+
+        // Slide Taux de mutation
+        JLabel labelMutationRate = new JLabel("Taux de mutation : " + mutationRate);
+        JSlider slideMutationRate = new JSlider();
+        slideMutationRate.setMaximum(20);
+        slideMutationRate.setMinimum(0);
+        slideMutationRate.setValue((int)(mutationRate*1000));
+        slideMutationRate.setPaintTicks(true);
+        slideMutationRate.setMinorTickSpacing(1);
+        slideMutationRate.setMajorTickSpacing(5);
+        slideMutationRate.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                labelMutationRate.setText("Taux de mutation: " + (double) ((JSlider) event.getSource()).getValue() / 1000.0);
+                mutationRate = (double)((JSlider) event.getSource()).getValue() / 1000.0;
+            }
+        });
+
+        // ------ Boutons run -------
         JButton runButton = new JButton("Lancement de l'algorithme");
         this.add(runButton);
 
-        // Labels
-        // Choix des données
+        // --------- Résultats ----------
         JPanel panResult = new JPanel();
         panResult.setBorder(BorderFactory.createTitledBorder("Résultats"));
         panResult.setPreferredSize(new Dimension(380, 300));
@@ -101,14 +249,111 @@ public class Controls extends JPanel {
         panResult.add(labelBestSolution);
         this.add(panResult, BorderLayout.NORTH);
 
-        // Listener bouton
+        // Listener RadioBox choix des algorithmes
+
+        // Algo Tabou
+        algo1RadioButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                // On retire les slides précédentes
+                panConfig.remove(labelMutationRate);
+                panConfig.remove(slideMutationRate);
+                panConfig.remove(labelArenaSize);
+                panConfig.remove(slideArenaSize);
+                panConfig.remove(labelNbIterationsAG);
+                panConfig.remove(slideNbIterationsAG);
+                panConfig.remove(labelPop);
+                panConfig.remove(slidePop);
+                panConfig.remove(labelNbIterationsRS);
+                panConfig.remove(slideNbIterationsRS);
+                panConfig.remove(labelStartingTemperature);
+                panConfig.remove(slideStartingTemperature);
+                panConfig.remove(labelCoolingRate);
+                panConfig.remove(slideCoolingRate);
+                panConfig.remove(labelNbIterationsTaboo);
+                panConfig.remove(slideNbIterationsTaboo);
+
+                // ajoute les nouvelles
+                panConfig.add(labelNbIterationsTaboo);
+                panConfig.add(slideNbIterationsTaboo);
+                panConfig.updateUI();
+            }
+        });
+
+        // Algo Recuit-Simulé
+        algo2RadioButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                // On retire les slides précédentes
+                panConfig.remove(labelMutationRate);
+                panConfig.remove(slideMutationRate);
+                panConfig.remove(labelArenaSize);
+                panConfig.remove(slideArenaSize);
+                panConfig.remove(labelNbIterationsAG);
+                panConfig.remove(slideNbIterationsAG);
+                panConfig.remove(labelPop);
+                panConfig.remove(slidePop);
+                panConfig.remove(labelNbIterationsRS);
+                panConfig.remove(slideNbIterationsRS);
+                panConfig.remove(labelStartingTemperature);
+                panConfig.remove(slideStartingTemperature);
+                panConfig.remove(labelCoolingRate);
+                panConfig.remove(slideCoolingRate);
+                panConfig.remove(labelNbIterationsTaboo);
+                panConfig.remove(slideNbIterationsTaboo);
+
+                //On ajoutes les nouvelles
+                panConfig.add(labelNbIterationsRS);
+                panConfig.add(slideNbIterationsRS);
+                panConfig.add(labelStartingTemperature);
+                panConfig.add(slideStartingTemperature);
+                panConfig.add(labelCoolingRate);
+                panConfig.add(slideCoolingRate);
+                panConfig.updateUI();
+            }
+        });
+
+        // Algo Génétique
+        algo3RadioButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                // On retire les slides précédentes
+                panConfig.remove(labelMutationRate);
+                panConfig.remove(slideMutationRate);
+                panConfig.remove(labelArenaSize);
+                panConfig.remove(slideArenaSize);
+                panConfig.remove(labelNbIterationsAG);
+                panConfig.remove(slideNbIterationsAG);
+                panConfig.remove(labelPop);
+                panConfig.remove(slidePop);
+                panConfig.remove(labelNbIterationsRS);
+                panConfig.remove(slideNbIterationsRS);
+                panConfig.remove(labelStartingTemperature);
+                panConfig.remove(slideStartingTemperature);
+                panConfig.remove(labelCoolingRate);
+                panConfig.remove(slideCoolingRate);
+                panConfig.remove(labelNbIterationsTaboo);
+                panConfig.remove(slideNbIterationsTaboo);
+
+                //On ajoutes les nouvelles
+                panConfig.add(labelNbIterationsAG);
+                panConfig.add(slideNbIterationsAG);
+                panConfig.add(labelPop);
+                panConfig.add(slidePop);
+                panConfig.add(labelArenaSize);
+                panConfig.add(slideArenaSize);
+                panConfig.add(labelMutationRate);
+                panConfig.add(slideMutationRate);
+                panConfig.updateUI();
+            }
+        });
+
+
+        // Listener bouton run
         runButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 Route bestSolution;
 
-                if (algo1.isSelected()) {
+                if (algo1RadioButton.isSelected()) {
                     bestSolution = runTabou();
-                } else if (algo2.isSelected()) {
+                } else if (algo2RadioButton.isSelected()) {
                     bestSolution = runRS();
                 } else {
                     bestSolution = runAG();
@@ -132,31 +377,33 @@ public class Controls extends JPanel {
 
             public Route runTabou() {
                 Route route = new Route(cities);
-                Taboo taboo = new Taboo(route);
+                Taboo taboo = new Taboo(route,numberOfIterationsTaboo);
                 double bestDistance = taboo.run();
                 return taboo.getBestSolution();
             }
 
             public Route runAG() {
                 Route route = new Route(cities);
-                AG ag = new AG(route);
+                AG ag = new AG(route,mutationRate,arenaSize,numberOfIterationsAG,populationSize);
                 double bestDistance = ag.run();
                 return ag.getBestSolution();
             }
 
             public Route runRS() {
                 Route route = new Route(cities);
-                RS rs = new RS(route);
+                RS rs = new RS(route,startingTemperature,numberOfIterationsRS,coolingRate);
                 double bestDistance = rs.run();
                 return rs.getBestSolution();
             }
         });
 
+        // Listener bouton LoadFile
         loadButton.addActionListener(arg0 -> {
             cities = loadFile("cities.csv");
             setCities(cities);
         });
 
+        // Listener bouton execute script
         scriptButton.addActionListener(arg0 -> {
             try {
                 Process proc = Runtime.getRuntime().exec("bash cities.sh --size " + nbCities);
@@ -169,7 +416,17 @@ public class Controls extends JPanel {
                 System.out.println(e.getMessage());
             }
         });
+
+
+        // Listener Slider nombre de villes
+        slideNbCities.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent event) {
+                labelNbCities.setText("Nombre de villes : " + ((JSlider) event.getSource()).getValue());
+                nbCities = ((JSlider) event.getSource()).getValue();
+            }
+        });
     }
+
 
     public void setRoute(Route route) {
         this.roadMap.setRoute(route);
